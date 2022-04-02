441ee901bdeebe328369743871b91804619e767e
VonZeeple <vonzeeple@gmail.com>
2022-04-02 23:20:05 +0200

    cleaned IHangingWireAnchor

--- src/hangingwires/IHangingWireAnchor.cs
+++ src/hangingwires/IHangingWireAnchor.cs
@@ -1,10 +1,4 @@
 ﻿using signals.src.signalNetwork;
-using signals.src.transmission;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vintagestory.API.Common;
 using Vintagestory.API.MathTools;
 
