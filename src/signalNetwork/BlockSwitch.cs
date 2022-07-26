4757a11470474f46c8d478fb731eb6787dc89eb0
VonZeeple <vonzeeple@gmail.com>
2022-07-26 03:00:12 +0200

    code cleaning

--- src/signalNetwork/BlockSwitch.cs
+++ src/signalNetwork/BlockSwitch.cs
@@ -6,18 +6,6 @@ namespace signals.src.signalNetwork
 {
     class BlockSwitch : BlockConnection
     {
-        public override void OnLoaded(ICoreAPI api)
-        {
-            base.OnLoaded(api);
-            api.Logger.Debug("switch block loaded");
-        }
-
-        public override void OnBlockPlaced(IWorldAccessor world, BlockPos blockPos, ItemStack byItemStack = null)
-        {
-            base.OnBlockPlaced(world, blockPos, byItemStack);
-            this.api.Logger.Debug("switch block placed");
-        }
-
         public override bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
         {
             BESwitch be = world.BlockAccessor.GetBlockEntity(blockSel.Position) as BESwitch;
