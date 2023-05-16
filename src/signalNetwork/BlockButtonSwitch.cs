1e67c83c219ad206fe287cf529781b7c020922b3
VonZeeple <vonzeeple@gmail.com>
2023-05-16 22:43:14 +0200

    Made button switch activable

--- src/signalNetwork/BlockButtonSwitch.cs
+++ src/signalNetwork/BlockButtonSwitch.cs
@@ -1,11 +1,22 @@
 ﻿using signals.src.transmission;
 using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
 
 namespace signals.src.signalNetwork
 {
     class BlockButtonSwitch : BlockConnection
     {
         string sound = "effect/woodswitch";
+
+        public override void Activate(IWorldAccessor world, Caller caller, BlockSelection blockSel, ITreeAttribute activationArgs = null)
+        {
+            BESwitch be = world.BlockAccessor.GetBlockEntity(blockSel.Position) as BESwitch;
+            if(!(be is null)){
+                be.OnInteract();
+            }
+            base.Activate(world, caller, blockSel, activationArgs);
+        }
+
         public override bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
         {
             bool result = base.OnBlockInteractStart(world, byPlayer, blockSel);
