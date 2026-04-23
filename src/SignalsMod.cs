0957cba577b732c4ec52a7d0a860bb6070ac36b9
VonZeeple <vonzeeple@gmail.com>
2026-04-23 22:26:20 +0200

    chore: updated mod version.

--- src/SignalsMod.cs
+++ src/SignalsMod.cs
@@ -14,7 +14,7 @@ using Vintagestory.GameContent;
     "signals",
     Description = "Wires, triodes and more.",
     Website = "",
-    Version = "0.2.8",
+    Version = "0.3.0",
     Authors = new[] { "PFev" })]
 
 namespace signals.src
