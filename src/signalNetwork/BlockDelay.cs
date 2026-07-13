38ab7ac50f449667bcc7688a1abed5f660bb47a0
Roze <roze.doyanawa@gmail.com>
2026-07-13 17:48:13 +0200

    fix:Delay blocks not clickable (issue #28)

--- src/signalNetwork/BlockDelay.cs
+++ src/signalNetwork/BlockDelay.cs
@@ -12,27 +12,32 @@ namespace signals.src.signalNetwork
             base.OnLoaded(api);
         }
 
-        public override bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
-        {
-            if(!base.OnBlockInteractStart(world, byPlayer, blockSel)) return false;
+        public override bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel) {
+            bool result = base.OnBlockInteractStart(world, byPlayer, blockSel);
+            if (blockSel.SelectionBoxIndex == GetSelectionBoxes(world.BlockAccessor, blockSel.Position).Length - 1) {
+                BEDelay be = world.BlockAccessor.GetBlockEntity(blockSel.Position) as BEDelay;
+                if (!(be is null)) {
+                    BlockPos pos = blockSel.Position;
+                    String sound = Attributes["triggerSound"].AsString();
+                    if (sound != null) {
+                        world.PlaySoundAt(AssetLocation.Create(sound), pos.X + 0.5f, pos.Y + 0.5f, pos.Z + 0.5f, byPlayer);
+                    }
 
-            BlockPos pos = blockSel.Position;
-            String sound = Attributes["triggerSound"].AsString();
-            if(sound != null){
-                world.PlaySoundAt(AssetLocation.Create(sound), pos.X + 0.5f, pos.Y + 0.5f, pos.Z + 0.5f, byPlayer);
+                    AssetLocation newCode;
+                    try {
+                        int value = Int32.Parse(this.Variant["value"]);
+                        newCode = CodeWithVariant("value", ((value + 1) % 6).ToString());
+                        Block newBlock = world.BlockAccessor.GetBlock(newCode);
+                        world.BlockAccessor.ExchangeBlock(newBlock.BlockId, pos);
+                        world.BlockAccessor.MarkBlockDirty(pos);
+                    } catch (Exception) {
+                        api.Logger.Debug("Can't parse variant for delay.");
+                    }
+
+                    return true;
+                }
             }
-            AssetLocation newCode;
-            try {
-                int value = Int32.Parse(this.Variant["value"]);
-                newCode = CodeWithVariant("value", ((value + 1) % 6).ToString());
-                Block newBlock = world.BlockAccessor.GetBlock(newCode);
-                world.BlockAccessor.ExchangeBlock(newBlock.BlockId, pos);
-                world.BlockAccessor.MarkBlockDirty(pos);
-            }
-            catch (Exception){
-                api.Logger.Debug("Can't parse variant for delay.");
-            };
-            return true;
+            return result;
         }
 
         public override string GetPlacedBlockInfo(IWorldAccessor world, BlockPos pos, IPlayer forPlayer)
